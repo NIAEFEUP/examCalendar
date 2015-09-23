d1b871ba5f77dc77939d6fbb489cbd402c1a242e
joaoBordalo <bordalo.joao94@gmail.com>
2015-09-23 19:49:52 +0100

    Info

---
+++ projeto/escreverNaBD.pl
@@ -0,0 +1,19 @@
+%receber do web site o ficheiro com os estudantes inscritos e colocar no formato:
+% estudante(NomeEstudante,NumeroEstudante,AnoInscricao).
+% estudanteCadeira(NumeroEstudante, CodigoCadeira).
+%e colocar em estudanteBD.pl
+
+% receber do web site o ficheiro com a descricao das cadeiras:
+% cadeira(NomeCadeira,CodigoCadeira,AnoCadeira,SemestreCadeira,Exame).
+%e colocar em cadeirasBD.pl
+
+
+%receber do web site o ficheiro com a descriçao das salas de exames:
+%sala(NumeroSala,Computador,lotacao).
+%e colocar em salaBD.pl
+
+
+%receber do web site o ficheiro com os professores:
+%professor(NomeProfessor,NumeroProfessor).
+%professorCadeira(NumeroProfessor,CodigoCadeira).
+%e colocar em professorBD.pl
---
+++ projeto/gerador.pl
@@ -0,0 +1,13 @@
+%restriçoes:
+% Cadeiras sem exame nao interessam
+% Salas pc e normais
+% Preferencia bloco da manha
+% 3 blocos possiveis: 9 / 13:30 / 17
+% Intervalo de marcacao de exames da mesma cadeira variavel, no minimo 14 dias entre a primeira e segunda epoca
+% Maior intrevalo entre exames de anos consecutivos( minimo 2 dias)
+% Classificar exames em termos de dificuldades
+% Prever indisponibilidade de professores ou problemas externos
+% Minimizar numero de estudantes com exames simultaneos ou proximos
+% Professores com mais que uma cadeira de exame, nao podem ter os exames na mesma hora
+% Ter em conta a lotacao das salas
+%www
