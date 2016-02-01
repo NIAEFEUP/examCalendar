07385d535bd37d6e7dfd851c863e63541d31a417
António Ramadas <up201303568@gcloud.fe.up.pt>
2016-02-01 21:37:26 +0000

    Corrigido diretório dos ficheiros

---
+++ projeto/Interface Web/constrains.php
@@ -0,0 +1,15 @@
+<?php
+include 'Templates/header.html';
+?>
+
+<div class="page-header">
+  <h1>Lista de restrições</h1>
+</div>
+
+<button type="button" class="btn btn-default">
+  <span class="glyphicon glyphicon-plus-sign" aria-hidden="true"> Adicionar Restrição</span>
+</button>
+
+<?php
+include 'Templates/footer.html';
+?>
