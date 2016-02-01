0cdd9856c014a0d43c4079885be981131b86442b
António Ramadas <up201303568@gcloud.fe.up.pt>
2016-02-01 21:09:37 +0000

    Uso pequeno do bootstrap

---
+++ projeto/Interface Web/login.php
@@ -0,0 +1,12 @@
+<?php
+include 'Templates/header.html';
+?>
+
+<div class="input-group input-group-lg">
+  <span class="input-group-addon" id="sizing-addon1">@</span>
+  <input type="text" class="form-control" placeholder="Username" aria-describedby="sizing-addon1">
+</div>
+
+<?php
+include 'Templates/footer.html';
+?>
