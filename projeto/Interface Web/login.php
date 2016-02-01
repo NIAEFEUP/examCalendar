b7a2f5701bfb4fc37ee327f8a8286c853dc8445c
António Ramadas <up201303568@gcloud.fe.up.pt>
2016-02-01 21:15:37 +0000

    Update no login

--- projeto/Interface Web/login.php
+++ projeto/Interface Web/login.php
@@ -3,7 +3,12 @@ include 'Templates/header.html';
 ?>
 
 <div class="input-group input-group-lg">
-  <span class="input-group-addon" id="sizing-addon1">@</span>
+  <span class="input-group-addon" id="sizing-addon1"></span>
+  <input type="text" class="form-control" placeholder="Username" aria-describedby="sizing-addon1">
+</div>
+
+<div class="input-group input-group-lg">
+  <span class="input-group-addon" id="sizing-addon1"></span>
   <input type="text" class="form-control" placeholder="Username" aria-describedby="sizing-addon1">
 </div>
 
