e919c296a2f75b95c49797ec5649193f2fab04a7
Pedro Fraga <antoniopedrofraga@gmail.com>
2016-02-03 19:51:14 +0000

    uploadUrl will tell the server wich way to go

--- projeto/Interface Web/importBdAux/importBd.js
+++ projeto/Interface Web/importBdAux/importBd.js
@@ -1,50 +1,12 @@
 
+/* Plugin utilizado: http://plugins.krajee.com/file-input */
 
 $(document).on('ready', function() {
 	
-	$("#teachers").fileinput({
+	$('.uploadFile').fileinput({
 		showUpload: true,
-		layoutTemplates: {
-			main1: "{preview}\n" +
-			"<div class=\'input-group {class}\'>\n" +
-			"   <div class=\'input-group-btn\'>\n" +
-			"       {browse}\n" +
-			"       {upload}\n" +
-			"       {remove}\n" +
-			"   </div>\n" +
-			"   {caption}\n" +
-			"</div>"
-		}
-	});
-
-	$("#students").fileinput({
-		showUpload: true,
-		layoutTemplates: {
-			main1: "{preview}\n" +
-			"<div class=\'input-group {class}\'>\n" +
-			"   <div class=\'input-group-btn\'>\n" +
-			"       {browse}\n" +
-			"       {upload}\n" +
-			"       {remove}\n" +
-			"   </div>\n" +
-			"   {caption}\n" +
-			"</div>"
-		}
-	});
-
-	$("#exam").fileinput({
-		showUpload: true,
-		layoutTemplates: {
-			main1: "{preview}\n" +
-			"<div class=\'input-group {class}\'>\n" +
-			"   <div class=\'input-group-btn\'>\n" +
-			"       {browse}\n" +
-			"       {upload}\n" +
-			"       {remove}\n" +
-			"   </div>\n" +
-			"   {caption}\n" +
-			"</div>"
-		}
+		uploadUrl: 'upload.php',
+		dropZoneEnabled: false
 	});
 	
 });
--- projeto/Interface Web/import_bd_view.php
+++ projeto/Interface Web/import_bd_view.php
@@ -8,13 +8,13 @@ include 'Templates/header.html';
 <div class="row">
 	<div class="col-lg-offset-3 col-lg-6">
 		<label class="control-label">Ficheiro de estudantes inscritos na UC</label>
-		<input id="students" name="input23[]" type="file" multiple class="file-loading">
+		<input class="uploadFile" id="students" name="input23[]" type="file" multiple class="file-loading">
 		<br>
 		<label class="control-label">Ficheiro de professores que lecionam a UC</label>
-		<input id="teachers" name="input23[]" type="file" multiple class="file-loading">
+		<input class="uploadFile" id="teachers" name="input23[]" type="file" multiple class="file-loading">
 		<br>
 		<label class="control-label">Ficheiro de salas de exame</label>
-		<input id="exam" name="input23[]" type="file" multiple class="file-loading">
+		<input class="uploadFile" id="exam" name="input23[]" type="file" multiple class="file-loading">
 		<br>
 	</div>
 </div>
@@ -23,7 +23,6 @@ include 'Templates/header.html';
 <?php
 include 'Templates/footer.html';
 ?>
-
 
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
