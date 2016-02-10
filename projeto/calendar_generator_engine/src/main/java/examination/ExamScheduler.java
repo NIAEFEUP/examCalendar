cb1d4eea1f63a587c160b616f97b6804c6302ec4
Gustavo Silva <gtugablue@gmail.com>
2016-02-10 18:23:11 +0000

    Created starting example (but causes an error because it has no exams).

--- projeto/calendar_generator_engine/src/main/java/examination/ExamScheduler.java
+++ projeto/calendar_generator_engine/src/main/java/examination/ExamScheduler.java
@@ -1,8 +1,22 @@
 package examination;
 
+import examination.domain.Examination;
+import org.optaplanner.core.api.solver.Solver;
+import org.optaplanner.core.api.solver.SolverFactory;
+
 /**
  * Created by Duarte on 10/02/2016.
  */
 public class ExamScheduler {
+    public static void main(String [] args){
+        System.out.println("Main");
+        SolverFactory solverFactory = SolverFactory.createFromXmlResource("examinationSolverConfig.xml");
+        Solver solver = solverFactory.buildSolver();
 
+        Examination unsolvedExamination = new Examination();
+        solver.solve(unsolvedExamination);
+        Examination solvedExamination = (Examination) solver.getBestSolution();
+
+        System.out.println("aaa");
+    }
 }
