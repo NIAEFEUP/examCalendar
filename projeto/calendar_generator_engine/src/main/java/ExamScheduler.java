e102225fccb1befa912dcfaf2f6ce9ead9cb378e
444Duarte <duartemr.pinto@gmail.com>
2016-02-10 17:12:13 +0000

    generator_engine- Initial draft

---
+++ projeto/calendar_generator_engine/src/main/java/ExamScheduler.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Duarte on 10/02/2016.
+ */
+public class ExamScheduler {
+    public static void main(String [] args){
+        System.out.println("Main");
+    }
+}
---
+++ projeto/calendar_generator_engine/src/main/java/examination/ExamScheduler.java
@@ -0,0 +1,8 @@
+package examination;
+
+/**
+ * Created by Duarte on 10/02/2016.
+ */
+public class ExamScheduler {
+
+}
