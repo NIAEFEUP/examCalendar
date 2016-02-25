bfeb1ad3010868802b81a1c0adb63edbdf9e710e
444Duarte <duartemr.pinto@gmail.com>
2016-02-25 16:49:41 +0000

    Generator-ProfConstraint. Added necessary classes

--- projeto/calendar_generator_engine/src/main/java/examination/domain/Professor.java
+++ projeto/calendar_generator_engine/src/main/java/examination/domain/Professor.java
@@ -1,5 +1,8 @@
 package examination.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Duarte
  * @version 1.0
@@ -8,11 +11,51 @@ package examination.domain;
 public class Professor {
 
 	private String id;
-	public Auxiliar m_Auxiliar;
-	public Regent m_Regent;
+
+	public List<Auxiliar> auxliarList;
+	public List<Regent> regentList;
+	List<ProfessorUnavailable> unavailableList;
 
 	public Professor(){
+		unavailableList = new ArrayList<ProfessorUnavailable>();
+		auxliarList = new ArrayList<Auxiliar>();
+		regentList = new ArrayList<Regent>();
+	}
 
+	public void addAsAuxiliar(Auxiliar auxiliar){
+		this.auxliarList.add(auxiliar);
+	}
+
+	public void addAsRegent(Regent regent){
+		this.regentList.add(regent);
+	}
+
+	public void addUnavailability(ProfessorUnavailable unavailability){
+		this.unavailableList.add(unavailability);
+	}
+
+	public List<ProfessorUnavailable> getUnavailableList() {
+		return unavailableList;
+	}
+
+	public void setUnavailableList(List<ProfessorUnavailable> unavailableList) {
+		this.unavailableList = unavailableList;
+	}
+
+	public List<Regent> getRegentList() {
+		return regentList;
+	}
+
+	public void setRegentList(List<Regent> regentList) {
+		this.regentList = regentList;
+	}
+
+	public List<Auxiliar> getAuxliarList() {
+		return auxliarList;
+	}
+
+	public void setAuxliarList(List<Auxiliar> auxliarList) {
+		this.auxliarList = auxliarList;
 	}
 
 	public void finalize() throws Throwable {
