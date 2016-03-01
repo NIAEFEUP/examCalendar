25a452f639e282d0134b316c9f84f534a9dc1f6c
Gustavo Silva <gtugablue@gmail.com>
2016-03-01 18:58:06 +0000

    Created "gapBetweenSameTopicExams" constraint and improved overall performance.

--- projeto/calendar_generator_engine/src/main/java/examination/domain/RoomPeriod.java
+++ projeto/calendar_generator_engine/src/main/java/examination/domain/RoomPeriod.java
@@ -11,10 +11,20 @@ import org.optaplanner.core.api.domain.variable.PlanningVariable;
  */
 @PlanningEntity(difficultyComparatorClass = RoomPeriodDifficultyComparator.class)
 public class RoomPeriod {
-
+	private static int currId = 0;
+	private int id;
 	private Room room;
 	public Exam exam;
 	private Period period;
+
+	public RoomPeriod() {
+		this.attributeId();
+	}
+
+	private void attributeId(){
+		this.currId++;
+		this.id = this.currId;
+	}
 
 	@PlanningVariable(nullable = true,valueRangeProviderRefs = {"examRange"})
 	public Exam getExam() {
@@ -22,9 +32,6 @@ public class RoomPeriod {
 	}
 	public void setExam(Exam exam){
 		this.exam = exam;}
-	public void finalize() throws Throwable {
-
-	}
 
 	public Period getPeriod() {
 		return period;
@@ -34,13 +41,16 @@ public class RoomPeriod {
 		this.period = period;
 	}
 
-
 	public Room getRoom() {
 		return room;
 	}
 
 	public void setRoom(Room room) {
 		this.room = room;
+	}
+
+	public int getId() {
+		return id;
 	}
 
 	@Override
