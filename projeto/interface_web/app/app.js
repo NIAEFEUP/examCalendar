f2dd8c8817bdea09b700e9d427245e07f660a520
António Ramadas <up201303568@gcloud.fe.up.pt>
2016-07-01 19:24:24 +0100

    AngularJS implementation

---
+++ projeto/interface_web/app/app.js
@@ -0,0 +1,25 @@
+var app = angular.module('ExamCalendar', ['ngRoute']);
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider
+  .when('/login', {
+    templateUrl: 'views/login/'
+  })
+  .when('/calendar', {
+    templateUrl: 'views/calendarOverview/'
+  })
+  .when('/constrains', {
+    templateUrl: 'views/constrains/'
+  })
+  .when('/adminHome', {
+    templateUrl: 'views/adminHome/'
+  })
+  .when('/adminUsers', {
+    templateUrl: 'views/adminUsers/'
+  })
+  .otherwise({
+    redirectTo: '/login'
+  });
+
+  $locationProvider.html5Mode(true);
+
+}]);
