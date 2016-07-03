07b736b4c6ca8e83870e6b6deab74204c6b86c5d
444Duarte <duartemr.pinto@gmail.com>
2016-07-04 00:34:48 +0100

    Added support for PRODEI UC's and other similar UC code's

--- projeto/calendar_generator_engine/src/main/java/parser/ExcelRegex.java
+++ projeto/calendar_generator_engine/src/main/java/parser/ExcelRegex.java
@@ -4,7 +4,7 @@ package parser;
  * Created by Duarte on 01/07/2016.
  */
 public class ExcelRegex {
-    public static final String UC_ID ="([A-z]{1,5}\\d{4})";
+    public static final String UC_ID ="([A-z]{1,8}\\d{4})";
     public static final String UC = "^"+UC_ID+"\\s*-\\s*(.+)$";
 
     public static final String CLASS = "^Turma\\s*:\\s*(\\d)[A-z]{1,7}\\w*$";
