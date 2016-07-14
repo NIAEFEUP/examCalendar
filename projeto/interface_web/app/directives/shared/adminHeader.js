78a4edcf0a86d7d663739bb7170dc60e06849dfa
António Ramadas <up201303568@gcloud.fe.up.pt>
2016-07-14 15:23:24 +0100

    Angular integration

--- projeto/interface_web/app/directives/shared/adminHeader.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-     $("#adminHeader").load("directives/shared/adminHeader.html");
-});
--- projeto/interface_web/app/views/adminUsers/script.js
+++
@@ -1,8 +0,0 @@
-function addUser() {
-	var constrain = $('<div class="userEmail"></div>');
-
-	constrain.append('<span class="emailContent">5. emailexample@email.com</span>');
-	constrain.append('<button class="btn btn-primary" type="button">Delete</button><br><br><br> ');
-
-	$('.col-sm-8').append(constrain);
-}
