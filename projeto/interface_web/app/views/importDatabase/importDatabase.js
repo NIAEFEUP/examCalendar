449bde04a79cd5539a7b6dd324bd96f5ff5fccd4
Gustavo Silva <silva95gustavo@gmail.com>
2016-08-04 19:47:31 +0100

    Added user interface to choose the normal and appeal seasons before importing the database.

--- projeto/interface_web/app/views/importDatabase/importDatabase.js
+++ projeto/interface_web/app/views/importDatabase/importDatabase.js
@@ -7,11 +7,12 @@ $(document).ready( function() {
 		autoFocus: true,
 		onStepChanging: function (event, currentIndex, newIndex)
 		{
-			if (currentIndex == 0) { // Import database
+			if (currentIndex == 1 && newIndex == currentIndex + 1) { // Import database
 				// TODO submit files
 				$('#pleaseWaitDialog').modal();
+				return false;
 			}
-			return false;
+			return true;
 		},
 		onFinishing: function (event, currentIndex)
 		{
@@ -33,6 +34,38 @@ $(document).ready( function() {
 	console.log($(this).parent().parent().prev().html());
 	var label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
 	$(this).parent().parent().prev().val(label); // Fill label with the name of the uploaded file.
+	});
+	
+	// Date range picker
+	var normalRangePicker = $('input[name="normal-season-date-range"]');
+	var appealRangePicker = $('input[name="appeal-season-date-range"]')
+	var rangePickers = $('input[name="normal-season-date-range"], input[name="appeal-season-date-range"]');
+	normalRangePicker.daterangepicker({
+		autoUpdateInput: false,
+		locale: {
+			format: 'DD-MM-YYYY'
+		}
+		}, function(start, end, label) {
+		
+			var date = new Date( Date.parse( end ) ); 
+			date.setDate( date.getDate() + 1 );
+			var newDate = date.toDateString(); 
+			newDate = new Date( Date.parse( newDate ) );
+			
+			appealRangePicker.daterangepicker({
+				autoUpdateInput: false,
+				minDate: newDate,
+				locale: {
+					format: 'DD-MM-YYYY'
+				}
+			});
+			appealRangePicker.prop("disabled", false);
+	});
+	rangePickers.on('apply.daterangepicker', function(ev, picker) {
+		$(this).val(picker.startDate.format('DD/MM/YYYY') + ' - ' + picker.endDate.format('DD/MM/YYYY'));
+	});
+	rangePickers.on('cancel.daterangepicker', function(ev, picker) {
+		$(this).val('');
 	});
 });
 
