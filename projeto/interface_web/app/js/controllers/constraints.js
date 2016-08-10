21a3e1b86869da0352a7c0409bb2e89d62e79d42
António Ramadas <up201303568@gcloud.fe.up.pt>
2016-08-10 15:44:02 +0100

    3 Constraint types added

--- projeto/interface_web/app/js/controllers/constraints.js
+++ projeto/interface_web/app/js/controllers/constraints.js
@@ -1,13 +1,38 @@
-app.controller('ConstraintsController', ['$scope', '$compile', function($scope, $compile) {
-  var constraintId = 1;
-  $scope.ola = "cenas";
-  $scope.addConstraint = function() {
+var rooms, professors, years, period;
+
+app.controller('ConstraintsController', ['$scope', '$compile', 'constraints', function($scope, $compile, constraints) {
+  constraints.success(function(data) {
+    rooms = data.rooms;
+    professors = data.professors;
+    years = data.years;
+    period = data.period;
+  });
+
+  var constraintId = 0;
+  $scope.addConstraint = function(type) {
+
+    var options = '';
+
+    switch(type) {
+      case 'Room':
+        options = html(rooms);
+        break;
+      case 'Professor':
+        options = html(professors);
+        break;
+      default: //'Year'
+        options = html(years);
+        break;
+    }
+
+    options += datesHTML();
+
     //var btnhtml = '<constraint id="' + constraintId + '"></constraint>';
     var btnhtml = '<constraint id="' + constraintId + '">'+
-    '<h3>Constraint Example'+
+    '<h3>'+type+' Constraint '+
       '<button class="btn btn-primary" type="button" onclick="removeConstraint('+constraintId+')"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>'+
-      '<button class="btn btn-primary" type="button"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>'+
-      '<button class="btn btn-primary" type="button"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>'+
+      options+
+      //'<button class="btn btn-primary" type="button"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>'+
     '</h3></constraint>';
     //var temp = $compile(btnhtml)($scope);
     //angular.element(document.getElementById('constraints')).append(temp);
@@ -16,13 +41,29 @@ app.controller('ConstraintsController', ['$scope', '$compile', function($scope, 
   };
 }]);
 
+function html(array) {
+  var html = '<select>';
+  array.forEach(function (item, index) {
+    if (item.id != null)
+      html += ('<option id="' + item.id + '">'+item.name+'</option>');
+    else
+      html += ('<option id="' + item + '">'+item+'</option>');
+  });
+  html += '</select> ';
+  return html;
+};
+
+function datesHTML() {
+  console.log(2);
+  return "2";
+};
+
 function removeConstraint(id) {
   var index = 1;
   $('#constraints > constraint').each(function () {
-    console.log(id);
     if ($(this).attr('id') == id) {
-      console.log(index);
       $('#constraints > constraint:nth-child('+index+')').remove();
+      return;
     } else {
       index++;
     }
