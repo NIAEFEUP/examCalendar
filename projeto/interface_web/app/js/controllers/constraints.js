aacd53600a72f74b633db4941cbfe2bcdd10db22
Gustavo Silva <silva95gustavo@gmail.com>
2016-08-11 02:03:08 +0100

    Added period pickers when creating constraints.

--- projeto/interface_web/app/js/controllers/constraints.js
+++ projeto/interface_web/app/js/controllers/constraints.js
@@ -25,13 +25,18 @@ app.controller('ConstraintsController', ['$scope', '$compile', 'constraints', fu
     }
 
     options += datesHTML();
+	var startPicker = generateConstraintPeriodRangePicker('start');
+	var endPicker = generateConstraintPeriodRangePicker('end');
 
-    var btnhtml = '<constraint id="constraint' + constraintId + '">'+
+    var btnhtml = '<constraint id="constraint' + constraintId + '" class="form-inline">'+
     '<h3>'+type+' Constraint '+
       '<button class="btn btn-primary" type="button" onclick="removeConstraint('+constraintId+')"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>'+
       options+
+	  'Start:'+startPicker+
+	  'End:'+endPicker+
       '</h3></constraint>';
     $('#constraints').append(btnhtml);
+	runDateTimePicker($('#constraint' + constraintId + ' [name="start-period"]'), $('#constraint' + constraintId + ' [name="end-period"]'));
     constraintId++;
   };
 }]);
@@ -59,3 +64,31 @@ function removeConstraint(id) {
   //TODO remove constraint from the server
   $('#constraint'+id).remove();
 };
+
+function generateConstraintPeriodRangePicker(type) {
+	return '<div class="input-group">'+
+					'<input type="text" class="form-control" name="'+type+'-period" value="" required/>'+
+					'<span class="input-group-addon">'+
+						'<span class="glyphicon glyphicon-calendar"></span>'+
+					'</span>'+
+				'</div>';
+}
+
+function runDateTimePicker(startElement, endElement) {
+	startElement.datetimepicker({
+		datepicker:true,
+		format:'d/m/Y H:i',
+		defaultTime:'9:00',
+		allowTimes:[
+		'9:00', '13:00', '17:00'
+		]
+	});
+	endElement.datetimepicker({
+		datepicker:true,
+		format:'d/m/Y H:i',
+		defaultTime:'9:00',
+		allowTimes:[
+		'9:00', '13:00', '17:00'
+		]
+	});
+}
