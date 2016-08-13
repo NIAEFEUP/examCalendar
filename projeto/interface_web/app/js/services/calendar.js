e08750d5c8c232056ad08786b0091b1b150f50e9
António Ramadas <up201303568@gcloud.fe.up.pt>
2016-08-13 02:38:47 +0100

    The calendar shown is the one from MySQL
    AngularJS + NodeJS + MySQL

--- projeto/interface_web/app/js/services/calendar.js
+++ projeto/interface_web/app/js/services/calendar.js
@@ -1,6 +1,5 @@
 app.factory('calendar', ['$http', function($http) {
   //TODO replace link
-  return $http.get('http://localhost/examCalendar/app/calendar.json')
   .success(function(data) {
     return data;
   })
