66a44a1cadd321fdf15e6546043ea3b660aa30f0
Gustavo Silva <silva95gustavo@gmail.com>
2016-08-19 17:15:38 +0100

    Server now updates the startTime and the endTime when processing an optimization request.

--- projeto/calendar_generator_engine/src/main/java/examcalendar/server/Scheduler.java
+++ projeto/calendar_generator_engine/src/main/java/examcalendar/server/Scheduler.java
@@ -7,33 +7,46 @@ import examcalendar.server.handlers.EvaluateRequestHandler;
 import org.optaplanner.core.api.solver.Solver;
 import org.optaplanner.core.api.solver.SolverFactory;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  * Created by Gustavo on 08/07/2016.
  */
 public class Scheduler extends Thread {
     private Server server;
-    private int requestID;
-    public Scheduler(Server server, int requestID) {
+    private int calendarID;
+    public Scheduler(Server server, int calendarID) {
         this.server = server;
-        this.requestID = requestID;
+        this.calendarID = calendarID;
     }
     @Override
     public void run() {
-        // TODO update request with the starting time
+        try {
+            Connection conn = server.createDatabaseConnection();
+            PreparedStatement ps = conn.prepareStatement("UPDATE calendars SET starttime = NOW() WHERE id = ?");
+            ps.setInt(1, calendarID);
+            ps.execute();
 
-        SolverFactory solverFactory = SolverFactory.createFromXmlResource("examinationSolverConfig.xml");
-        Solver solver = solverFactory.buildSolver();
+            SolverFactory solverFactory = SolverFactory.createFromXmlResource("examinationSolverConfig.xml");
+            Solver solver = solverFactory.buildSolver();
 
-        Examination unsolvedExamination = new ExaminationDBImporter(true).readSolution(requestID);
-        solver.solve(unsolvedExamination);
-        Examination solvedExamination = (Examination) solver.getBestSolution();
-        solvedExamination.removeNullPeriodsExams();
-        solvedExamination.sort();
-        System.out.println(solvedExamination);
-        System.out.println(EvaluateRequestHandler.evaluateSolution(solvedExamination));
+            Examination unsolvedExamination = new ExaminationDBImporter(true).readSolution(calendarID);
+            solver.solve(unsolvedExamination);
+            Examination solvedExamination = (Examination) solver.getBestSolution();
+            solvedExamination.removeNullPeriodsExams();
+            solvedExamination.sort();
+            System.out.println(solvedExamination);
+            System.out.println(EvaluateRequestHandler.evaluateSolution(solvedExamination));
 
-        // TODO export solution to the database and update request with the ending time
-        new ExaminationDBExporter(true).writeSolution(solvedExamination, requestID);
-        server.notifyEvent(Server.Event.SCHEDULER_END);
+            new ExaminationDBExporter(true).writeSolution(solvedExamination, calendarID);
+            ps = conn.prepareStatement("UPDATE calendars SET endtime = NOW() WHERE id = ?");
+            ps.setInt(1, calendarID);
+            ps.execute();
+            server.notifyEvent(Server.Event.SCHEDULER_END);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
