27b6fff2b9bbeb29aaf1caab045f9ab14af836d8
Gustavo Silva <silva95gustavo@gmail.com>
2016-09-08 02:33:25 +0100

    Small optimization.

--- projeto/calendar_generator_engine/src/main/java/examcalendar/optimizer/domain/solver/ExamMoveFactory.java
+++ projeto/calendar_generator_engine/src/main/java/examcalendar/optimizer/domain/solver/ExamMoveFactory.java
@@ -33,8 +33,8 @@ public class ExamMoveFactory implements MoveListFactory<Examination> {
                     if (rp.getPeriod().equals(period) && rp.getExam() == null) {
                         capacity += rp.getRoom().getCapacity();
                         newRPs.add(rp);
+                        if (capacity >= exam.getNumStudents()) break;
                     }
-                    if (capacity >= exam.getNumStudents()) break;
                 }
                 if (capacity < exam.getNumStudents() || oldRPs.size() == 0) continue;
                 moveList.add(new ExamMove(exam, oldRPs, newRPs));
