4557f1ff8b7eb4b3ba51ca3f3d482c32332de44d
Gustavo Silva <silva95gustavo@gmail.com>
2016-09-23 01:51:06 +0100

    When dragging exams between periods, the period information is now obtainable. To implement: send the new period info to the database.

--- projeto/interface_web/app/js/directives/calendar.js
+++ projeto/interface_web/app/js/directives/calendar.js
@@ -1,10 +1,12 @@
+var redipsInit;
+
 app.directive('calendar', function($timeout) {
   return {
     restrict : 'E',
     templateUrl: 'directives/calendar/calendar.html',
     link: function (scope, element, attrs) {
       $timeout(function() {
-        REDIPS.drag.init();
+		redipsInit();
       });
     }
   };
@@ -23,3 +25,23 @@ app.directive('modal', function() {
     templateUrl: 'directives/calendar/modal.html'
   };
 });
+
+// redips initialization
+redipsInit = function () {
+	// reference to the REDIPS.drag library and message line
+	var	rd = REDIPS.drag;
+	// how to display disabled elements
+	rd.style.borderDisabled = 'solid';	// border style for disabled element will not be changed (default is dotted)
+	rd.style.opacityDisabled = 60;		// disabled elements will have opacity effect
+	// initialization
+	rd.init();
+	rd.event.dropped = function (targetCell) {
+		var examID = $(rd.obj).data("id");
+		var slot = $(targetCell);
+		var date = slot.data("date") || null;
+		var time = slot.data("time");
+		if (typeof time == 'undefined')
+			time = null;
+		console.log(date, time);
+	};
+};
